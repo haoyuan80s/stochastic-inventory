b0e41f0a733a808aabe449587c99637baf50daa1
haoyuan80s <haoyuan@0587336696.wireless.umich.net>
2014-12-15 16:51:11 -0500

    Hao make some correction on main.cpp

--- code/IC_Support.h
+++ code/IC_Support.h
@@ -53,11 +53,11 @@ void Explanation(){
 
 void DC_Exp(){
 	cout<<"Welcome to use the DataCollector of the stochastic inventory control."<<endl;
-	cout<<"In your inputfile, there should be just one number in the first line specify how many data sets you are going to generate. ";
+	cout<<"In your input file, there should be just one number in the first line specify how many data sets you are going to generate. ";
 	cout<<"Every data set should be separated by an empty new line.";
 	cout<<endl;
 	cout<<"For every data set, please specify the following parameters: "<<endl;
-	cout<<" 0. You want to collect averaging data of sample path for given parameters OR you want to collect datas for a range of different PLANNING HORIZON. ";
+	cout<<" 0. You want to collect averaging data of sample path for given parameters OR you want to collect data for a range of different PLANNING HORIZON. ";
 	cout<<"(Put 1 if you want to do the first, 2 for the second.)"<<endl;
 	cout<<endl;
 
@@ -65,15 +65,15 @@ void DC_Exp(){
 	cout<<" 1. How many iterations you want to compute to get the average data;(int)"<<endl;
 	cout<<" 2. What is your planning horizon;(int)"<<endl;
 	cout<<" 3. What is your leading time;(int)"<<endl;
-	cout<<" 4. What is your discount rate and initial inventory;(alpha double, between 0 and 1,inventory double)"<<endl;
+	cout<<" 4. What is your discount rate and initial inventory;(alpha double, between 0 and 1, inventory double)"<<endl;
 	cout<<" 5. Is this a integer typed problem or not;(1 for yes, 0 for no)"<<endl;
 	cout<<" 6. What is the algorithm; (1: DP, 2: Myopic, 3: Dual Balancing)"<<endl;
-	cout<<" 7. Waht is your distribution type? (1: Binomial, 2: RA1 Bernoulli, 3: ind Normal)"<<endl;
-	cout<<" 8. Waht are your distribution parameters?"<<endl;
-	cout<<" 9. Whether your cost coefficient are going to be constant; (1 for const, 0 for vector)"<<endl;
-	cout<<" 10,11,12. Specify three vectors or three numbers depending on 9.(order is buying cost,holding cost then backlogging cost.)"<<endl;
+	cout<<" 7. What is your distribution type? (1: Binomial, 2: AR1 Bernoulli, 3: ind Normal, 4: AR1 Normal, 5: ARn Normal)"<<endl;
+	cout<<" 8. What are your distribution parameters?"<<endl;
+	cout<<" 9. Whether your cost coefficient are going to be constant; (1 for constant, 0 for vector)"<<endl;
+	cout<<" 10,11,12. Specify three vectors or three numbers depending on 9.(order is buying cost, holding cost then backlogging cost.)"<<endl;
 	cout<<endl;
-	cout<<"If your answer is 2 for parameter 0, then for prameter 2(planning horizon), specify a number indicate how many different horizons, and follow by a vector"<<endl;
+	cout<<"If your answer is 2 for parameter 0, then for parameter 2(planning horizon), specify a number indicate how many different horizons, and follow by a vector"<<endl;
 	cout<<endl;
 
 }
